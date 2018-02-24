6f10974180f8319749a10f79a7b08e0f2f5b6fd1
Ruslan <qwertysakkal@gmail.com>
2018-02-24 22:42:32 +0400

    Configured url mapping with search input. Removed outline on search button focus

--- src/components/SearchFilms/SearchFilms.jsx
+++ src/components/SearchFilms/SearchFilms.jsx
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
+import { Redirect, Switch, Route } from 'react-router-dom'; 
+
 import styles from './SearchFilms.scss';
 
 export default class SearchFilms extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            fireRedirect: false
+        };
+    }
+
+    componentDidMount() {
+        this.input.value = this.props.match.params.query || '';
+    }
+
+    submitForm(e) {
+        e.preventDefault();
+
+        this.setState({
+            fireRedirect: true,
+            query: this.input.value
+        });
+    } 
+
     render() {
-        console.log(this.props.match.params.query);
+        const { fireRedirect, query } = this.state;
+
         return (
-            <form action="/search/" method="GET" className="SearchFilms-form">
+            <form action="/search/" method="GET" className="SearchFilms-form" onSubmit={(e) => this.submitForm(e)}>
                 <label htmlFor="search-movie-input">Find your movie</label>
-                <input type="text" id="search-movie-input" name="query" className="SearchFilms-input" />
+                <input type="text" id="search-movie-input" className="SearchFilms-input" name="query" ref={node => this.input = node}/>
                 <div className="SearchFilms-buttons">
                     <button type="submit" className="SearchFilms-submit-btn">Search</button>
                 </div>
+                {fireRedirect && <Redirect to={`/search/${query}`} />}
             </form>
         );
     }
