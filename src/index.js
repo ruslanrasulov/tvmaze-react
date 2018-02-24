879a193be7b4d52d5aa628e1c456a99f6f338da0
Ruslan <qwertysakkal@gmail.com>
2018-02-24 22:41:20 +0400

    Added root routing for App component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import App from './components/App/App.jsx';
 import styles from './index.scss';
 
 ReactDOM.render(
     <Router>
-        <App />
+        <Route path="/" component={App} />
     </Router>,
     document.getElementById('root')
 );
