3f455a65218b57fb8b96ea6f43b6daeba9bf1569
Unknown <ruslanrasulov31@gmail.com>
2018-03-02 00:42:52 +0400

    Configured mapping for action payloads

--- src/modules/configuration/reducers.js
+++ src/modules/configuration/reducers.js
@@ -1,13 +1,35 @@
 import { combineReducers } from 'redux';
+import { map } from 'lodash';
+
 import * as actionTypes from './acitonTypes.js';
 
+const mapFilmInfo = ({ data }) => ({
+    id: data.id,
+    name: data.name,
+    genres: data.genres,
+    year: data.premiered,
+    imageUrl: data.image.medium
+});
+
+const mapSearchResults = ({ data }) => (
+    map(data, ({ show }) => ({
+        id: show.id,
+        name: show.name,
+        genres: show.genres,
+        year: show.premiered,
+        imageUrl: show.image.medium,
+        duration: show.runtime,
+        director: "Quentin Tarantino"
+    }))
+);
+
 export default combineReducers({
-    shows: (state = { name: 'eboy'}, action) => {
+    shows: (state = { }, action) => {
         switch (action.type) {
             case actionTypes.SEARCH_FILMS_BY_NAME:
-                return {...state, searchResults: action.payload };
+                return {...state, searchResults: mapSearchResults(action.payload) };
             case actionTypes.SHOW_FILM_INFO_BY_ID:
-                return {...state, filmInfo: action.payload };
+                return {...state, filmInfo: mapFilmInfo(action.payload) };
             default: 
                 return state;
         }
